82c61d11b0e022a3f0a575ae74f1c4f0f123f71b
BillySmithDesign <josh@bullsharkinvestments.com>
2026-08-09 18:40:14 +0930

    1.0

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+export const SALON_ID =
+  process.env.OPENBOOK_SALON_ID ||
+  '11111111-1111-1111-1111-111111111111';
+
+export const TZ =
+  process.env.OPENBOOK_TIMEZONE ||
+  'Australia/Adelaide';
